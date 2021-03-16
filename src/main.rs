fe1c52683408fb134e54abeaefc9cfaeefa75dd9
Diego Fioravanti <fioravanti.diego@gmail.com>
2021-03-16 06:06:56 +0100

    added basic logging functionalities

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use env_logger::Env;
 use std::net::TcpListener;
 
 use sqlx::PgPool;
@@ -5,6 +6,10 @@ use zero2prod::{configuration::get_configuration, startup::run};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // set up the logger. If the RUST_LOG env variable is set then it uses that level of logging
+    // otherwise it defaults to info.
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let configuration = get_configuration().expect("Failed to read configuration");
     let address = format!("127.0.0.1:{}", configuration.application_port);
 
